8aac5c68e5d5ba75e568ec5a0b44e32d285b0ff5
mbengvw <mbeng@uniku.ac.id>
2024-07-29 17:15:31 +0700

    Perbaikan tampilan data kehadiran

--- resources/views/presensi/list_presensi.blade.php
+++ resources/views/presensi/list_presensi.blade.php
@@ -7,82 +7,68 @@
                 Kehadiran Harian Siswa
             </h2>
         </div>
-
-        <div class="row">
-            <div class="col-sm-4" style="margin-bottom: 30px;">
-                <div class="card">
-                    <div class="card-header">
-                        <h4>Parameter Filter</h4>
-                    </div>
-                    <div class="card-body">
-                        <form>
-                            <div class="form-group">
-                                <label for="inputEmail3" class="col-sm col-form-label">Kelas</label>
-                                <div class="col-sm">
+        <div class="card" style="margin-bottom: 20px">
+            <div class="card-header">
+                Filter Data
+            </div>
+            <div class="card-body">
+                <form>
+                    <div class="row" style="padding-bottom: 20px">
+                        <div class="col-sm-10">
+                            <div class="form-row">
+                                <div class="col">
                                     <select class="form-control" id="select_kelas" name="select_kelas">
-                                        <option value="">Semua</option>
+                                        <option value="">Pilih Kelas</option>
                                         @foreach ($list_kelas as $kelas)
                                             <option value="{{ $kelas['id_kelas'] }}">{{ $kelas['nama_kelas'] }}</option>
                                         @endforeach
                                     </select>
                                 </div>
-                            </div>
-
-                            <div class="form-group">
-                                <label for="inputEmail3" class="col-sm col-form-label">Tahun</label>
-                                <div class="col-sm">
+                                <div class="col">
                                     <select class="form-control" id="select_tahun" name="select_tahun">
-                                        <option value="">Semua</option>
+                                        <option value="">Pilih Tahun</option>
                                         @for ($i = 2022; $i < 2030; $i++)
-                                            <option value={{ $i }}>{{ $i }}
-                                                @if ($data_tahun->tahun == $i)
-                                                    selected
-                                                @endif
-                                            </option>
+                                            @if ($data_tahun->tahun == $i)
+                                                <option value={{ $i }} selected>{{ $i }}</option>
+                                            @else
+                                                <option value={{ $i }}>{{ $i }}</option>
+                                            @endif
                                         @endfor
                                     </select>
                                 </div>
-                            </div>
-
-                            <div class="form-group">
-                                <label for="inputEmail3" class="col-sm col-form-label">Semester</label>
-                                <div class="col-sm">
+                                <div class="col">
                                     <select class="form-control" id="select_semester" name="select_semester">
-                                        <option value="">Semua</option>
+                                        <option value="">Pilih Semester</option>
                                         <option value="1">Ganjil</option>
                                         <option value="2">Genap</option>
                                     </select>
                                 </div>
-                            </div>
-
-                            <div class="form-group">
-                                <label for="inputTanggal" class="col-sm col-form-label">Tanggal</label>
-                                <div class="col-sm">
-                                    <input type="text" class="form-control tanggal" name="tanggal" id="tanggal">
+                                <div class="col">
+                                    <input type="text" class="form-control tanggal" name="tanggal" id="tanggal"
+                                        placeholder="Tanggal">
+                                </div>
+                                <div class="col">
+                                    <input type="text" class="form-control" name="nama" id="nama"
+                                        placeholder="Nama Siswa">
+                                </div>
+                                <div class="col">
                                 </div>
                             </div>
-
-                            <div class="form-group">
-                                <label for="inputnama" class="col-sm col-form-label">Nama Siswa</label>
-                                <div class="col-sm">
-                                    <input type="text" class="form-control" name="nama" id="nama">
-                                </div>
-                            </div>
-
-                            <div class="form-group">
-                                <div class="col-sm-10">
-                                    <button type="submit" class="btn btn-primary" id="showbtn" style="margin-right:50px;">Show Data</button>
-                                    <a href="{{ route('piket.index') }}" role="button" class="btn btn-warning">Home</a>
-                                </div>
-                            </div>
-                        </form>
+                        </div>
                     </div>
-
-                </div>
-
+                    <div class="row">
+                        <div class="col-sm-10">
+                            <button type="submit" class="btn btn-primary" id="showbtn" style="margin-right:50px;">Show
+                                Data</button>
+                            <a href="{{ route('piket.index') }}" role="button" class="btn btn-warning">Home</a>
+                        </div>
+                    </div>
+                </form>
             </div>
+        </div>
 
-            <div class="col-sm-8">
+        <div class="row">
+            <div class="col-sm-12">
                 <div class="card">
                     <div class="card-header">
                         Data Kehadiran
