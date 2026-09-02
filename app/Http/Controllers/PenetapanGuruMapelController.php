d8bce92b465983724e677f86c4cfc91938bc5235
Mbeng <mbengmerzy@gmail.com>
2026-09-02 17:47:28 +0700

    perbaikan list guru mapel

--- app/Http/Controllers/PenetapanGuruMapelController.php
+++ app/Http/Controllers/PenetapanGuruMapelController.php
@@ -29,13 +29,13 @@ class PenetapanGuruMapelController extends Controller
                 })
                 ->make(true);
         }
-        
+
         $mapel = MstMapel::all();
         $kelas = Kelas::where('id_tahun', $tahun->id)->get();
         // Assuming role 'guru' or 'admin' or just fetch users 
         // For simplicity, fetch users not admin and not siswa
-        $guru = User::whereDoesntHave('roles', function($q) {
-            $q->whereIn('name', ['admin', 'siswa']);
+        $guru = User::whereDoesntHave('roles', function ($q) {
+            $q->whereIn('name', ['siswa']);
         })->get();
 
         return view('master.penetapan_guru_mapel.index', compact('mapel', 'kelas', 'guru'));
@@ -52,23 +52,23 @@ class PenetapanGuruMapelController extends Controller
     {
         $mapel = MstMapel::find($request->id_mapel);
         $tahun = TahunService::getActive();
-        
+
         $query = Kelas::where('id_tahun', $tahun->id);
-        
+
         if ($mapel && $mapel->tingkat) {
             $query->where('tingkat', $mapel->tingkat);
         }
         if ($mapel && $mapel->jurusan && strtolower($mapel->jurusan) != 'umum') {
             $query->where('jurusan', $mapel->jurusan);
         }
-        
+
         // Exclude classes that are already assigned to this mapel in the current academic year
         $assignedKelasIds = PenetapanGuruMapel::where('id_mapel', $request->id_mapel)
-                                              ->where('id_tahun', $tahun->id)
-                                              ->pluck('id_kelas');
-                                              
+            ->where('id_tahun', $tahun->id)
+            ->pluck('id_kelas');
+
         $query->whereNotIn('id_kelas', $assignedKelasIds);
-        
+
         $kelas = $query->get();
         return response()->json($kelas);
     }
@@ -107,9 +107,9 @@ class PenetapanGuruMapelController extends Controller
         } else {
             foreach ($request->id_kelas as $kelas_id) {
                 $exists = PenetapanGuruMapel::where('id_tahun', $tahun->id)
-                            ->where('id_kelas', $kelas_id)
-                            ->where('id_mapel', $request->id_mapel)
-                            ->first();
+                    ->where('id_kelas', $kelas_id)
+                    ->where('id_mapel', $request->id_mapel)
+                    ->first();
                 if ($exists) {
                     $exists->update(['id_guru' => $request->id_guru]);
                 } else {
